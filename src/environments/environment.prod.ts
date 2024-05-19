6e06850f015dd1e527164a917b7651e89f9fc7ef
nijaigiri <nijaigiri472002@gmail.com>
2024-05-19 18:07:25 +0530

    added firebase api key to prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   firebase: {
-    // Your firebase config here
-  },
+    apiKey: "AIzaSyAlvPDFwxPKj6mqxHNUFVqdU5lx3trqIrI",
+    authDomain: "cipherconnect-86426.firebaseapp.com",
+    projectId: "cipherconnect-86426",
+    storageBucket: "cipherconnect-86426.appspot.com",
+    messagingSenderId: "455225425063",
+    appId: "1:455225425063:web:a73d522b62c973e5102f82"
+},
   production: true,
 };
